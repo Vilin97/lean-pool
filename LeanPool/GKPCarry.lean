c2c08a59eb3a322184f02ab07ca664861a25b250
Egor Lyfar <egor.lyfar@gmail.com>
2026-07-21 20:03:03 +0800

    feat: formalize GKP carry-language characterization

---
+++ LeanPool/GKPCarry.lean
@@ -0,0 +1,20 @@
+/-
+Copyright (c) 2026 Egor Lyfar. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Egor Lyfar
+-/
+
+import LeanPool.GKPCarry.BadCarryCounting
+import LeanPool.GKPCarry.FiniteRangeCorollary
+import LeanPool.GKPCarry.GKPCharacterization
+
+/-!
+# A Carry-Language Characterization of the Graham--Knuth--Patashnik Conjecture
+
+Source: url:https://cs.stanford.edu/~knuth/gkp.html
+Authors: Egor Lyfar
+Status: verified
+Main declarations: `GKPCarry.gkpConjecture_iff_badCarryLanguageExclusion`
+Tags: number-theory, finite-automata
+MSC: 11A63, 11B65, 68Q45
+-/
