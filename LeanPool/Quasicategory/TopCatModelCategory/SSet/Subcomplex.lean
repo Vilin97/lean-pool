9168f2462656222a7a8fd320e30fd417fba075d7
Vasily Ilin <vasilin97@gmail.com>
2026-07-31 18:18:55 +0000

    wip: remove 61 upstreamed duplicate decls from Subcomplex (Subpresheaf->Subfunctor migration remains)

--- LeanPool/Quasicategory/TopCatModelCategory/SSet/Subcomplex.lean
+++ LeanPool/Quasicategory/TopCatModelCategory/SSet/Subcomplex.lean
@@ -18,7 +18,6 @@ import LeanPool.Quasicategory.TopCatModelCategory.ColimitsType
 import LeanPool.Quasicategory.TopCatModelCategory.CommSq
 import LeanPool.Quasicategory.TopCatModelCategory.SSet.Basic
 
-
 /-!
 # Subcomplexes of simplicial sets
 
@@ -165,19 +164,6 @@ variable (S : X.Subcomplex) (T : Y.Subcomplex)
 
 variable (X)
 
-@[simps!]
-def topIso : ((⊤ : X.Subcomplex) : SSet) ≅ X :=
-  NatIso.ofComponents (fun n ↦ (Equiv.Set.univ (X.obj n)).toIso)
-
-@[reassoc (attr := simp)]
-lemma topIso_inv_ι : (topIso X).inv ≫ Subpresheaf.ι _ = 𝟙 _ := rfl
-
-def isInitialBot : IsInitial ((⊥ : X.Subcomplex) : SSet.{u}) :=
-  IsInitial.ofUniqueHom (fun P ↦
-    { app i := fun ⟨x, hx⟩ ↦ by simp at hx
-      naturality i j f := by ext ⟨x, hx⟩; simp at hx })
-    (fun _ _ ↦ by ext _ ⟨x, hx⟩; simp at hx)
-
 variable {X}
 
 /-variable {S} in
@@ -211,40 +197,12 @@ lemma ι_app {Δ : SimplexCategoryᵒᵖ} (x : S.obj Δ) :
 instance : Mono S.ι := by
   infer_instance
 
-@[simps]
-noncomputable def prod : (X ⊗ Y).Subcomplex where
-  obj Δ := (S.obj Δ).prod (T.obj Δ)
-  map i _ hx := ⟨S.map i hx.1, T.map i hx.2⟩
-
-lemma prod_monotone {S₁ S₂ : X.Subcomplex} (hX : S₁ ≤ S₂) {T₁ T₂ : Y.Subcomplex} (hY : T₁ ≤ T₂) :
-    S₁.prod T₁ ≤ S₂.prod T₂ :=
-  fun _ _ hx => ⟨hX _ hx.1, hY _ hx.2⟩
-
 example : PartialOrder X.Subcomplex := inferInstance
 example : SemilatticeSup X.Subcomplex := inferInstance
 
 section
 
 variable {S₁ S₂ : X.Subcomplex} (h : S₁ ≤ S₂)
-
-def homOfLE : (S₁ : SSet.{u}) ⟶ (S₂ : SSet.{u}) := Subpresheaf.homOfLe h
-
-@[reassoc]
-lemma homOfLE_comp {S₃ : X.Subcomplex} (h' : S₂ ≤ S₃) :
-    homOfLE h ≫ homOfLE h' = homOfLE (h.trans h') := rfl
-
-variable (S₁) in
-@[simp]
-lemma homOfLE_refl : homOfLE (by rfl : S₁ ≤ S₁) = 𝟙 _ := rfl
-
-@[simp]
-lemma homOfLE_app_val (Δ : SimplexCategoryᵒᵖ) (x : S₁.obj Δ) :
-    ((homOfLE h).app Δ x).val = x.val := rfl
-
-@[reassoc (attr := simp)]
-lemma homOfLE_ι : homOfLE h ≫ S₂.ι = S₁.ι := rfl
-
-instance mono_homOfLE : Mono (homOfLE h) := mono_of_mono_fac (homOfLE_ι h)
 
 end
 
@@ -269,26 +227,6 @@ variable (X) in
 def forget : X.Subcomplex ⥤ SSet.{u} where
   obj S := S
   map f := homOfLE (leOfHom f)
-
-noncomputable def unionProd : (X ⊗ Y).Subcomplex := ((⊤ : X.Subcomplex).prod T) ⊔ (S.prod ⊤)
-
-lemma mem_unionProd_iff {n : SimplexCategoryᵒᵖ} (x : (X ⊗ Y).obj n) :
-    x ∈ (unionProd S T).obj _ ↔ x.1 ∈ S.obj _ ∨ x.2 ∈ T.obj _ := by
-  dsimp [unionProd, Set.prod]
-  aesop
-
-lemma top_prod_le_unionProd : (⊤ : X.Subcomplex).prod T ≤ S.unionProd T := le_sup_left
-
-lemma prod_top_le_unionProd : (S.prod ⊤) ≤ S.unionProd T := le_sup_right
-
-lemma prod_le_top_prod : S.prod T ≤ (⊤ : X.Subcomplex).prod T :=
-  prod_monotone le_top (by rfl)
-
-lemma prod_le_prod_top : S.prod T ≤ S.prod ⊤ :=
-  prod_monotone (by rfl) le_top
-
-lemma prod_le_unionProd : S.prod T ≤ S.unionProd T :=
-  (prod_le_prod_top S T).trans (prod_top_le_unionProd S T)
 
 end Subcomplex
 
@@ -316,34 +254,12 @@ lemma subcomplexHorn_toSSet (n : ℕ) (i : Fin (n + 1)) :
 lemma subcomplexHorn_ι (n : ℕ) (i : Fin (n + 1)) :
     (subcomplexHorn.{u} n i).ι = hornInclusion n i := rfl-/
 
-lemma mem_horn_iff {n : ℕ} (i : Fin (n + 1)) {m : SimplexCategoryᵒᵖ}
-    (x : (Δ[n] : SSet.{u}).obj m) :
-    x ∈ (horn n i).obj m ↔ Set.range (stdSimplex.asOrderHom x) ∪ {i} ≠ Set.univ := Iff.rfl
-
-@[simp]
-lemma boundary_zero : boundary.{u} 0 = ⊥ := by
-  ext m x
-  simp [boundary]
-  intro x
-  fin_cases x
-  refine ⟨0, ?_⟩
-  apply Subsingleton.elim
-
 section
 
 variable {X Y}
 variable (f : X ⟶ Y)
 
 attribute [local simp] NatTrans.naturality_apply
-
-abbrev Subcomplex.range : Y.Subcomplex := Subpresheaf.range f
-
-/-@[simps]
-def Subcomplex.range : Y.Subcomplex where
-  obj Δ := Set.range (f.app Δ)
-  map := by
-    rintro Δ Δ' φ _ ⟨x, rfl⟩
-    exact ⟨X.map φ x, by simp⟩-/
 
 @[simp]
 lemma Subcomplex.range_ι (A : X.Subcomplex) :
@@ -369,14 +285,6 @@ instance : Balanced SSet.{u} :=
 instance {X Y : SSet.{u}} (f : X ⟶ Y) [Mono f] : IsIso (toRangeSubcomplex f) := by
   have := mono_of_mono_fac (toRangeSubcomplex_ι f)
   apply isIso_of_mono_of_epi
-
-lemma Subcomplex.range_eq_top_iff : Subcomplex.range f = ⊤ ↔ Epi f := by
-  rw [NatTrans.epi_iff_epi_app, Subpresheaf.ext_iff, funext_iff]
-  simp only [epi_iff_surjective, Subpresheaf.range_obj, Subpresheaf.top_obj, Set.top_eq_univ,
-    Set.range_eq_univ]
-
-lemma Subcomplex.range_eq_top [Epi f] : Subcomplex.range f = ⊤ := by
-  rwa [range_eq_top_iff]
 
 end
 
@@ -409,60 +317,13 @@ lemma obj (n : SimplexCategoryᵒᵖ) :
     rw [← sq.min_eq]
     rfl
 
-lemma isPushout : IsPushout (homOfLE sq.le₁₂) (homOfLE sq.le₁₃)
-    (homOfLE sq.le₂₄) (homOfLE sq.le₃₄) where
-  w := rfl
-  isColimit' := ⟨by
-    refine evaluationJointlyReflectsColimits _ (fun n ↦ ?_)
-    exact (PushoutCocone.isColimitMapCoconeEquiv _ _).2
-      (Types.isColimitPushoutCoconeOfBicartSqOfSets (sq.obj n))⟩
-
 end Sq
 
 section
 
-variable {Y}
-@[simps]
-def preimage (A : X.Subcomplex) (p : Y ⟶ X) : Y.Subcomplex where
-  obj n := p.app n ⁻¹' (A.obj n)
-  map f := (Set.preimage_mono (A.map f)).trans (by
-    simp only [Set.preimage_preimage, NatTrans.naturality_apply _ _ p f]
-    rfl)
-
-@[simp]
-lemma preimage_max (A B : X.Subcomplex) (p : Y ⟶ X) :
-    (A ⊔ B).preimage p = A.preimage p ⊔ B.preimage p := rfl
-
-@[simp]
-lemma preimage_min (A B : X.Subcomplex) (p : Y ⟶ X) :
-    (A ⊓ B).preimage p = A.preimage p ⊓ B.preimage p := rfl
-
-@[simp]
-lemma preimage_iSup {ι : Type*} (A : ι → X.Subcomplex) (p : Y ⟶ X) :
-    (⨆ i, A i).preimage p = ⨆ i, (A i).preimage p := by aesop
-
-@[simp]
-lemma preimage_iInf {ι : Type*} (A : ι → X.Subcomplex) (p : Y ⟶ X) :
-    (⨅ i, A i).preimage p = ⨅ i, (A i).preimage p := by aesop
-
-@[simps]
-def fromPreimage (A : X.Subcomplex) (p : Y ⟶ X) :
-    (A.preimage p : SSet) ⟶ (A : SSet) where
-  app Δ y := ⟨p.app _ y.1, y.2⟩
-  naturality _ _ f := by
-    ext ⟨y, hy⟩
-    dsimp
-    ext
-    exact NatTrans.naturality_apply _ _ p f y
-
 lemma ofSimplex_eq_range {X : SSet.{u}} {n : ℕ} (x : X _⦋n⦌) :
     Subcomplex.ofSimplex x = range (yonedaEquiv.symm x) := by
   simp only [Subcomplex.range_eq_ofSimplex, Equiv.apply_symm_apply]
-
-@[simp]
-lemma preimage_ι {X : SSet.{u}} (A : X.Subcomplex) :
-    A.preimage A.ι = ⊤ := by
-  aesop
 
 /-def ofSimplex {n : ℕ} (x : X _[n]) : X.Subcomplex :=
   range ((X.yonedaEquiv (.mk n)).symm x)-/
@@ -488,11 +349,6 @@ lemma ofSimplex_le_iff {n : ℕ} (x : X _[n]) (A : X.Subcomplex) :
     obtain ⟨f, rfl⟩ := (standardSimplex.objEquiv _ _).symm.surjective y
     exact A.map f.op h-/
 
-lemma mem_ofSimplex_obj_iff {n : ℕ} (x : X _⦋n⦌) {m : SimplexCategoryᵒᵖ} (y : X.obj m) :
-    y ∈ (ofSimplex x).obj m ↔ ∃ (f : m.unop ⟶ ⦋n⦌), X.map f.op x = y := by
-  simp [ofSimplex, Subpresheaf.ofSection]
-  aesop
-
 lemma le_ofSimplex_iff (x : X _⦋0⦌) (A : X.Subcomplex) :
     A ≤ ofSimplex x ↔ A.ι = const x := by
   constructor
@@ -512,50 +368,6 @@ end
 section
 
 variable {Y} (S : X.Subcomplex) (f : X ⟶ Y)
-
-abbrev image : Y.Subcomplex := Subpresheaf.image S f
-
-lemma image_le_iff (Z : Y.Subcomplex) :
-    S.image f ≤ Z ↔ S ≤ Z.preimage f := by
-  simp [Subpresheaf.le_def]
-
-lemma image_top : (⊤ : X.Subcomplex).image f = range f := by aesop
-
-lemma image_comp {Z : SSet.{u}} (g : Y ⟶ Z) :
-    S.image (f ≫ g) = (S.image f).image g := by aesop
-
-lemma range_comp {Z : SSet.{u}} (g : Y ⟶ Z) :
-    Subcomplex.range (f ≫ g) = (Subcomplex.range f).image g := by
-  simp only [← image_top, image_comp]
-
-lemma image_eq_range : S.image f = range (S.ι ≫ f) := by
-  simp only [range_comp, Subpresheaf.range_ι]
-
-lemma image_iSup {ι : Type*} (S : ι → X.Subcomplex) (f : X ⟶ Y) :
-    image (⨆ i, S i) f = ⨆ i, (S i).image f := by
-  aesop
-
-@[simp]
-lemma preimage_range : (range f).preimage f = ⊤ :=
-  le_antisymm (by simp) (by rw [← image_le_iff, image_top])
-
-lemma image_le_range : S.image f ≤ range f := by
-  simp only [image_le_iff, preimage_range, le_top]
-
-@[simp]
-lemma image_ofSimplex {n : ℕ} (x : X _⦋n⦌) (f : X ⟶ Y) :
-    (ofSimplex x).image f = ofSimplex (f.app _ x) := by
-  apply le_antisymm
-  · rw [image_le_iff, ofSimplex_le_iff, preimage_obj, Set.mem_preimage]
-    apply mem_ofSimplex_obj
-  · rw [ofSimplex_le_iff]
-    exact ⟨x, mem_ofSimplex_obj _, rfl⟩
-
-def toImage : (S : SSet) ⟶ (S.image f : SSet) :=
-  (S.image f).lift (S.ι ≫ f) (by simp [Subpresheaf.range_comp])
-
-@[reassoc (attr := simp)]
-lemma toImage_ι : S.toImage f ≫ (S.image f).ι = S.ι ≫ f := rfl
 
 instance : Epi (S.toImage f) := by
   rw [← range_eq_top_iff]
@@ -597,22 +409,6 @@ lemma preimage_eq_iff {X Y : SSet.{u}}
   rw [← mono_iff_injective]
   infer_instance
 
-lemma preimage_eq_top_iff {X Y : SSet.{u}}
-    (f : X ⟶ Y) (B : Y.Subcomplex) :
-    B.preimage f = ⊤ ↔ range f ≤ B := by
-  constructor
-  · rintro h _ x ⟨y, rfl⟩
-    exact h.symm.le _ (by simp)
-  · aesop
-
-@[simp]
-lemma preimage_image_of_isIso {X Y : SSet.{u}} (f : X ⟶ Y) (B : Y.Subcomplex) [IsIso f] :
-    (B.preimage f).image f = B := by
-  apply le_antisymm
-  · rw [image_le_iff]
-  · intro n y hy
-    exact ⟨(inv f).app _ y, by simpa [← NatTrans.comp_app_apply]⟩
-
 @[simp]
 lemma image_preimage_of_isIso {X Y : SSet.{u}} (f : X ⟶ Y) (A : X.Subcomplex) [IsIso f] :
     (A.image f).preimage f = A := by
@@ -626,16 +422,6 @@ end
 section
 
 variable {Y} (f : X ⟶ Y) {B : Y.Subcomplex} (hf : B.preimage f = ⊤)
-
-def lift : X ⟶ B :=
-  (topIso X).inv ≫ homOfLE (by simp [hf]) ≫ B.fromPreimage f
-
-@[reassoc (attr := simp)]
-lemma lift_ι : lift f hf ≫ B.ι = f := rfl
-
-@[simp]
-lemma lift_app_coe {n : SimplexCategoryᵒᵖ} (x : X.obj n) :
-    ((lift f g).app _ x).1 = f.app _ x := rfl
 
 end
 
@@ -717,44 +503,9 @@ noncomputable def isColimitPushoutCoconeOfPullback [hf : Mono f] :
 
 end
 
-variable {Y} in
-noncomputable def prodIso (S : X.Subcomplex) (T : Y.Subcomplex) :
-    (S.prod T : SSet) ≅ (S : SSet) ⊗ (T : SSet) where
-  hom := ChosenFiniteProducts.lift
-    (lift ((S.prod T).ι ≫ ChosenFiniteProducts.fst _ _) (by
-      ext n ⟨x, h⟩
-      simpa using h.1))
-    (lift ((S.prod T).ι ≫ ChosenFiniteProducts.snd _ _) (by
-      ext n ⟨x, h⟩
-      simpa using h.2))
-  inv := lift (S.ι ⊗ T.ι) (by
-    ext n ⟨x, y⟩
-    dsimp
-    simp only [Set.mem_preimage, tensorHom_app_apply, Subpresheaf.ι_app,
-      Set.mem_univ, iff_true]
-    exact ⟨x.2, y.2⟩)
-
 namespace unionProd
 
 variable {Y} (S : X.Subcomplex) (T : Y.Subcomplex)
-
-noncomputable def ι₁ : X ⊗ T ⟶ unionProd S T :=
-  lift (X ◁ T.ι) (by
-    ext m ⟨x₁, x₂⟩
-    simp [unionProd, Set.prod]
-    exact Or.inl x₂.2)
-
-noncomputable def ι₂ : (S : SSet.{u}) ⊗ Y ⟶ (unionProd S T : SSet.{u}) :=
-  lift (S.ι ▷ Y) (by
-    ext m ⟨x₁, x₂⟩
-    simp [unionProd, Set.prod]
-    exact Or.inr x₁.2)
-
-@[reassoc (attr := simp)]
-lemma ι₁_ι : ι₁ S T ≫ (unionProd S T).ι = X ◁ T.ι := rfl
-
-@[reassoc (attr := simp)]
-lemma ι₂_ι : ι₂ S T ≫ (unionProd S T).ι = S.ι ▷ Y := rfl
 
 lemma sq : Sq (S.prod T) ((⊤ : X.Subcomplex).prod T) (S.prod ⊤) (unionProd S T) where
   max_eq := rfl
@@ -763,42 +514,6 @@ lemma sq : Sq (S.prod T) ((⊤ : X.Subcomplex).prod T) (S.prod ⊤) (unionProd S
     change _ ∧ _ ↔ _
     simp [prod, Set.prod, Membership.mem, Set.Mem, setOf]
     tauto
-
-lemma isPushout : IsPushout (S.ι ▷ (T : SSet)) ((S : SSet) ◁ T.ι)
-    (unionProd.ι₁ S T) (unionProd.ι₂ S T) :=
-  (sq S T).isPushout.of_iso
-    (Subcomplex.prodIso _ _)
-    (Subcomplex.prodIso _ _ ≪≫ MonoidalCategory.whiskerRightIso (topIso X) _)
-    (Subcomplex.prodIso _ _ ≪≫ MonoidalCategory.whiskerLeftIso _ (topIso Y))
-    (Iso.refl _) rfl rfl rfl rfl
-
-@[simp]
-lemma preimage_β_hom : (unionProd S T).preimage (β_ _ _).hom = unionProd T S := by
-  ext n ⟨x, y⟩
-  simp only [mem_unionProd_iff, preimage_obj, Set.mem_preimage,
-    braiding_hom_apply_fst, braiding_hom_apply_snd]
-  tauto
-
-@[simp]
-lemma preimage_β_inv : (unionProd S T).preimage (β_ _ _).inv = unionProd T S := by
-  apply preimage_β_hom
-
-@[simp]
-lemma image_β_hom : (unionProd S T).image (β_ _ _).hom = unionProd T S := by
-  rw [← preimage_β_hom, preimage_image_of_isIso]
-
-@[simp]
-lemma image_β_inv : (unionProd S T).image (β_ _ _).hom = unionProd T S := by
-  apply image_β_hom
-
-
-noncomputable def symmIso : (unionProd S T : SSet) ≅ (unionProd T S : SSet) where
-  hom := lift ((unionProd S T).ι ≫ (β_ _ _).hom) (by
-    simp only [Subcomplex.preimage_eq_top_iff, range_comp, Subpresheaf.range_ι, image_β_hom,
-      le_refl])
-  inv := lift ((unionProd T S).ι ≫ (β_ _ _).hom) (by
-    simp only [Subcomplex.preimage_eq_top_iff, range_comp, Subpresheaf.range_ι, image_β_hom,
-      le_refl])
 
 end unionProd
 
