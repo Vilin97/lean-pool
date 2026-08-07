6f1ec17d905c9697d844579a1ea6f20c39fbfa29
Vasily Ilin <vasilin97@gmail.com>
2026-08-07 12:42:02 -0700

    import aggregate coverage choices for validity

--- LeanPool/Erdos97ConvexOctagon/CoverageSummaryChoiceValidity.lean
+++ LeanPool/Erdos97ConvexOctagon/CoverageSummaryChoiceValidity.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Egor Lyfar
 -/
 
+import LeanPool.Erdos97ConvexOctagon.CoverageSummaryChoices
 import LeanPool.Erdos97ConvexOctagon.CoverageSummaryChoicesValidity
 
 /-! # Validity audits for static coverage-summary placement -/
