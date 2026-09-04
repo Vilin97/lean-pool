658b93f49c8f36597b335ece0fe3d7abdb848a04
Vasily Ilin <vasilin97@gmail.com>
2026-09-04 06:21:17 +0000

    feat: import permanent formula lower bound

--- LeanPool.lean
+++ LeanPool.lean
@@ -3169,6 +3169,7 @@ import LeanPool.PentagonalNumberTheorem.Generic
 import LeanPool.PentagonalNumberTheorem.Old
 import LeanPool.PentagonalNumberTheorem.Partition
 import LeanPool.PentagonalNumberTheorem.PowerSeries
+import LeanPool.PermanentFormulaLowerBound
 import LeanPool.PhaseRetrieval
 import LeanPool.PhaseRetrieval.Constant
 import LeanPool.PhaseRetrieval.Constant.Internal.AnnulusLocalEstimate
