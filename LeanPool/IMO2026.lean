c177eed4b708b58397db69eb531f2cc14dd1ab31
Vasily Ilin <vasilin97@gmail.com>
2026-09-05 03:28:38 -0700

    Import IMO 2026 Question 6 formalization

---
+++ LeanPool/IMO2026.lean
@@ -0,0 +1,18 @@
+/-
+Copyright (c) 2026 Axiom Math. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Axiom Math
+-/
+
+import LeanPool.IMO2026.Q6
+
+/-!
+# Periodicity of the IMO 2026 common-factor sequence
+
+Source: url:https://github.com/AxiomMath/IMO2026
+Authors: Axiom Math
+Status: verified
+Main declarations: `IMO2026Q6.main_theorem`
+Tags: imo, number-theory, integer-sequences, periodicity, gcd
+MSC: 11B99, 11A05
+-/
