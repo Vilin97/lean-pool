d7bdb95c8ef78152a437acb3e092f569d363f145
Vasily Ilin <vasilin97@gmail.com>
2026-09-10 22:34:18 -0700

    Fix Lean Pool headers and card formatting

--- LeanPool/Lean4EVM.lean
+++ LeanPool/Lean4EVM.lean
@@ -1,6 +1,6 @@
 /-
 Copyright (c) 2026 mrLSD. All rights reserved.
-Released under MIT license as described in the file LICENSE.
+Released under Apache 2.0 license as described in the file LICENSE.
 Authors: mrLSD
 -/
 
@@ -13,7 +13,9 @@ import LeanPool.Lean4EVM.Primitives.FixedBytes
 Source: url:https://github.com/mrLSD/Lean4EVM
 Authors: mrLSD
 Status: verified
-Main declarations: `Lean4EVM.FixedUInt.toNat_add`, `Lean4EVM.FixedUInt.toNat_mul`, `Lean4EVM.U256.toNat_addmod`, `Lean4EVM.U256.toNat_mulmod`, `Lean4EVM.U256.toNat_byteAt_of_lt`
+Main declarations: `Lean4EVM.FixedUInt.toNat_add`, `Lean4EVM.FixedUInt.toNat_mul`,
+`Lean4EVM.U256.toNat_addmod`, `Lean4EVM.U256.toNat_mulmod`,
+`Lean4EVM.U256.toNat_byteAt_of_lt`
 Tags: verified-algorithms, ethereum, fixed-width-arithmetic, blockchain-semantics
 MSC: 68Q60, 68V20
 -/
--- LeanPool/Lean4EVM/Primitives/FixedBytes/H256.lean
+++ LeanPool/Lean4EVM/Primitives/FixedBytes/H256.lean
@@ -1,6 +1,6 @@
 /-
 Copyright (c) 2026 mrLSD. All rights reserved.
-Released under MIT license as described in the file LICENSE.
+Released under Apache 2.0 license as described in the file LICENSE.
 Authors: mrLSD
 -/
 import LeanPool.Lean4EVM.Primitives.FixedBytes.Core
