d9ad05d1cbf0ce18de219c8bb491802094b2e52c
Vasily Ilin <vasilin97@gmail.com>
2026-09-12 11:25:11 -0700

    Add license header to Besicovitch entry

--- LeanPool/Besicovitch.lean
+++ LeanPool/Besicovitch.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2026 Yongxi Lin. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Yongxi Lin
+-/
+
 import LeanPool.Besicovitch.BPC.Basic
 import LeanPool.Besicovitch.BPC.Defs
 import LeanPool.Besicovitch.BPC.Extraction
