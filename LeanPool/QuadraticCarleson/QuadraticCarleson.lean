69ed137aeef7d992269724b2ff6b3ad33bbc580b
Vasily Ilin <vasilin97@gmail.com>
2026-09-21 18:05:58 +0000

    Import Endpoint estimates for Stein's purely quadratic Carleson operator

---
+++ LeanPool/QuadraticCarleson/QuadraticCarleson.lean
@@ -0,0 +1,13 @@
+/-
+Copyright (c) 2026 Anastasios Fragkos. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Anastasios Fragkos
+-/
+
+import LeanPool.QuadraticCarleson.QuadraticCarleson.Basic
+
+/-!
+# Quadratic Carleson formalization
+
+Root import module for the completed formalization.
+-/
