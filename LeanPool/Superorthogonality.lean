e8c3be6515683bcd7b8b2aa5c6a3f6a74fcf16ec
Vasily Ilin <vasilin97@gmail.com>
2026-09-25 08:44:34 +0000

    Migrate Superorthogonality to explicit public Lean modules

--- LeanPool/Superorthogonality.lean
+++ LeanPool/Superorthogonality.lean
@@ -3,15 +3,17 @@ Copyright (c) 2026 Joris Roos. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joris Roos
 -/
+module
 
-import LeanPool.Superorthogonality.LeanSuperorthogonality.Codex.MainTheorem
-import LeanPool.Superorthogonality.LeanSuperorthogonality.Codex.PointwiseEstimate
-import LeanPool.Superorthogonality.LeanSuperorthogonality.Codex
-import LeanPool.Superorthogonality.LeanSuperorthogonality.Defs
-import LeanPool.Superorthogonality.LeanSuperorthogonality.MainTheorem
-import LeanPool.Superorthogonality.LeanSuperorthogonality.PointwiseEstimate
-import LeanPool.Superorthogonality.LeanSuperorthogonality
-import LeanPool.Superorthogonality.Solution
+
+public import LeanPool.Superorthogonality.LeanSuperorthogonality
+public import LeanPool.Superorthogonality.LeanSuperorthogonality.Codex
+public import LeanPool.Superorthogonality.LeanSuperorthogonality.Codex.MainTheorem
+public import LeanPool.Superorthogonality.LeanSuperorthogonality.Codex.PointwiseEstimate
+public import LeanPool.Superorthogonality.LeanSuperorthogonality.Defs
+public import LeanPool.Superorthogonality.LeanSuperorthogonality.MainTheorem
+public import LeanPool.Superorthogonality.LeanSuperorthogonality.PointwiseEstimate
+public import LeanPool.Superorthogonality.Solution
 
 /-!
 # Square-function estimates from type IV superorthogonality
